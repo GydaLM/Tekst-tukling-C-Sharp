fea94a81b4eca65de54e40f570851c7cc3753982
Lonatri <gyda-94@hotmail.com>
2025-11-26 10:12:11 +0100

    Add project files.

---
+++ Tekst tukling C#/Program.cs
@@ -0,0 +1,39 @@
+﻿
+TekstTukling();
+void TekstTukling()
+{
+    Console.WriteLine("1. Rotate text");
+    Console.WriteLine("2. Change word");
+    var keyInput = Console.ReadKey();
+
+    Console.Clear();
+    Console.WriteLine("Write what you want to change:");
+    var textInput = Console.ReadLine();
+
+    if (keyInput.KeyChar == '1')
+    {
+        Console.WriteLine(RotateText(textInput));
+    }
+    else if(keyInput.KeyChar == '2')
+    {
+        ChangeWord(textInput);
+    }
+    else
+    {
+        Console.WriteLine("Choose 1 or 2");
+    }
+}
+
+string RotateText(string input)
+{
+    char[] stringArray = input.ToCharArray();
+    Array.Reverse(stringArray);
+    string reversed = new string(stringArray);
+    return reversed;
+}
+
+void ChangeWord(string input)
+{
+    input = input.Replace('e', 'a');
+    Console.WriteLine($"Your altered text: {input}");
+}
